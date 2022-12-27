6634f7e0cf998c3f45fb4714329a5a6eefb6ca8e
Anatoliy Katelnitskiy <katel1998@yandex.ru>
2022-12-27 09:29:10 +0300

    text fix eslint

--- test/test.js
+++ test/test.js
@@ -1,44 +1,44 @@
-import getSpecialAttacks from "../src/getSpecialAttacks";
+import getSpecialAttacks from '../src/getSpecialAttacks';
 
 test('function test getSpecialAttacks', () => {
-    const character = {
-        name: 'Лучник',
-        type: 'Bowman',
-        health: 50,
-        level: 3,
-        attack: 40,
-        defence: 10,
-        special: [
-          {
-            id: 8,
-            name: 'Двойной выстрел',
-            icon: 'http://...',
-            description: 'Двойной выстрел наносит двойной урон'
-          }, 
-          {
-            id: 9,
-            name: 'Нокаутирующий удар',
-            icon: 'http://...'
-          }
-        ]	
-      }
+  const character = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон',
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+      },
+    ],
+  };
 
-    const rightResponse = [
-        {
-            id: 8,
-            name: 'Двойной выстрел',
-            icon: 'http://...',
-            description: 'Двойной выстрел наносит двойной урон' 
-        },
-        {
-            id: 9,
-            name: 'Нокаутирующий удар',
-            icon: 'http://...',
-            description: 'Описание недоступно'
-        }
-    ]
+  const rightResponse = [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно',
+    },
+  ];
 
-    const result = getSpecialAttacks(character);
+  const result = getSpecialAttacks(character);
 
-    expect(result).toEqual(rightResponse);
-})
+  expect(result).toEqual(rightResponse);
+});
